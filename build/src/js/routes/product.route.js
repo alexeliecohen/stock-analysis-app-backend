953c4dd75ea850dbed5d111c57348aa3031b153a
Alex <alex.toutongi@gmail.com>
2022-03-28 02:09:57 -0400

    Completed the dev portion of the demo project

--- build/src/js/routes/product.route.js
+++ build/src/js/routes/product.route.js
@@ -7,6 +7,7 @@ const router = (0, express_1.Router)();
 exports.router = router;
 const productService = new product_service_1.ProductService();
 router.post('/products/create', async (req, res) => {
+    console.log(req.body);
     const result = await productService.create(req);
     res.status(201).send(result);
 });
--- src/js/routes/product.route.ts
+++ src/js/routes/product.route.ts
@@ -5,12 +5,13 @@ const router = Router();
 const productService: ProductService = new ProductService();
 
 router.post('/products/create', async (req, res) => {
+    console.log(req.body)
     const result = await productService.create(req);
     res.status(201).send(result)
 })
 
 router.get('/products/get-all', async (req,res) => {
     const result = await productService.getAll(req);
-    res.send(result)
+    res.send(result);
 })
 export {router};
